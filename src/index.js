3295c4013e36884cf9692d651d816402f5b42995
IvanLF <ivaanlopez602@gmail.com>
2022-07-04 19:06:53 -0500

    feat: change format indexes are added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes,Route, } from "react-router-dom";
 import App from './App';
+import AllPokemones from './routes/AllPokemones';
+import Buscar from './routes/BuscarPokemonid';
+import Matricula from './routes/Matricula';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+      <Route path="pokemones" element={<AllPokemones />} />
+      <Route path="pokemonId" element={<Buscar />} />
+      <Route path="matricula" element={<Matricula />} />
+      </Route>
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
